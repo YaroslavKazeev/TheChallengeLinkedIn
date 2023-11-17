b2483523a09c66e3bce70571c8013988350b06d9
YaroslavKazeev <yaroslav.kazeev@outlook.com>
2023-11-17 15:38:59 +0100

    SignUpBranchWorksInViews

--- LinkedInApp/views.py
+++ LinkedInApp/views.py
@@ -5,56 +5,31 @@ from django.contrib.auth.models import User
 
 def signup_view(request):
     if request.method == 'POST':
-        print(request.POST)
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data.get('username')
-            job_title = form.cleaned_data.get('job_title')
-            if any(char.isdigit() for char in name) or any(char.isdigit() for char in job_title):
-                form.add_error('username', 'Name and job title should not contain numbers')
-            if len(name) < 4:
-                form.add_error('username', 'Name should be at least 4 characters long')
-            if len(job_title) < 14:
-                form.add_error('job_title', 'Job title should be at least 14 characters long')
-            if not form.errors:
-                user = form.save()
-                Profile.objects.create(user=user, job_title=job_title)
-                return redirect('members')
-    else:
-        form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
+        if 'signup_form' in request.POST:
+            form = SignUpForm(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data.get('username')
+                job_title = form.cleaned_data.get('job_title')
+                if any(char.isdigit() for char in name) or any(char.isdigit() for char in job_title):
+                    form.add_error('username', 'Name and job title should not contain numbers')
+                if len(name) < 4:
+                    form.add_error('username', 'Name should be at least 4 characters long')
+                if len(job_title) < 14:
+                    form.add_error('job_title', 'Job title should be at least 14 characters long')
+                if not form.errors:
+                    user = form.save()
+                    Profile.objects.create(user=user, job_title=job_title)
+                    return redirect('members')
 
 
 
 
-# def signup_view(request):
-#     if request.method == 'POST':
+        elif 'login_form' in request.POST:
+            print("Sign-in")
 
-#         print(request.POST)
-#         #if request.POST.get("id") == "signup_form":
-
-#         form = SignUpForm(request.POST)
-#         if form.is_valid():
-#             name = form.cleaned_data.get('username')
-#             job_title = form.cleaned_data.get('job_title')
-#             # email = form.cleaned_data.get('email')
-#             if any(char.isdigit() for char in name) or any(char.isdigit() for char in job_title):
-#                 form.add_error('username', 'Name and job title should not contain numbers')
-#             if len(name) < 4:
-#                 form.add_error('username', 'Name should be at least 4 characters long')
-#             if len(job_title) < 14:
-#                 form.add_error('job_title', 'Job title should be at least 14 characters long')
-#             if not form.errors:
-#                 user = form.save()
-#                 Profile.objects.create(user=user, job_title=job_title)
-#                 return redirect('members')
-#     else:
-#         form = SignUpForm(request.POST)
-# # login logic
-
-#     # return render(request, 'signup.html', {'form1': form1, 'form2': form2})
-#     return render(request, 'signup.html', {'form': form})
-
+        else:
+            form = SignUpForm()
+    # return render(request, 'signup.html', {'form': form})
 
 
 
@@ -69,7 +44,7 @@ def members_view(request):
 def profile_view(request, user_id):
     if request.user.is_authenticated:
         user = User.objects.get(id=user_id)
-        profile = Profile.objects.get(id=user_id)
+        profile = Profile.objects.get(id=user_id-2)
         return render(request, 'profile.html', {'username': user.username, 'job_title': profile.job_title})
     else:
         return redirect('signup')
