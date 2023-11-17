581d615596196a45a9401ac93bf3ec3bc85f8787
YaroslavKazeev <yaroslav.kazeev@outlook.com>
2023-11-17 15:54:22 +0100

    register&loginFormsAreSeparated

--- LinkedInApp/views.py
+++ LinkedInApp/views.py
@@ -6,31 +6,33 @@ from django.contrib.auth.models import User
 def signup_view(request):
     if request.method == 'POST':
         if 'signup_form' in request.POST:
-            form = SignUpForm(request.POST)
-            if form.is_valid():
-                name = form.cleaned_data.get('username')
-                job_title = form.cleaned_data.get('job_title')
+            register_form = SignUpForm(request.POST)
+            if register_form.is_valid():
+                name = register_form.cleaned_data.get('username')
+                job_title = register_form.cleaned_data.get('job_title')
                 if any(char.isdigit() for char in name) or any(char.isdigit() for char in job_title):
-                    form.add_error('username', 'Name and job title should not contain numbers')
+                    register_form.add_error('username', 'Name and job title should not contain numbers')
                 if len(name) < 4:
-                    form.add_error('username', 'Name should be at least 4 characters long')
+                    register_form.add_error('username', 'Name should be at least 4 characters long')
                 if len(job_title) < 14:
-                    form.add_error('job_title', 'Job title should be at least 14 characters long')
-                if not form.errors:
-                    user = form.save()
+                    register_form.add_error('job_title', 'Job title should be at least 14 characters long')
+                if not register_form.errors:
+                    user = register_form.save()
                     Profile.objects.create(user=user, job_title=job_title)
                     return redirect('members')
-
-
 
 
         elif 'login_form' in request.POST:
             print("Sign-in")
 
         else:
-            form = SignUpForm()
+            register_form = SignUpForm()
+            login_form = SignInForm(request)
     # return render(request, 'signup.html', {'form': form})
-
+    else:
+        register_form = SignUpForm()
+        login_form = SignInForm(request)
+    return render(request, 'signup.html', {'register_form': register_form, 'login_form': login_form})
 
 
 
